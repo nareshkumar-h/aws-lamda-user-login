d3d5c97997eb637f8d404266adbaf0e4feb72c4e
Naresh Kumar H <nareshkumarh@live.com>
2017-03-26 11:49:41 +0530

    Updated correct mailId

--- src/main/java/com/naresh/UserDAO.java
+++ src/main/java/com/naresh/UserDAO.java
@@ -8,7 +8,7 @@ public class UserDAO {
 	private static Map<Integer, User> usersMap = new HashMap<Integer, User>();
 
 	static {
-		usersMap.put(1, new User(1, "Naresh", "naresh@gmail.com", "pass"));
+		usersMap.put(1, new User(1, "Naresh", "nareshkumarh@live.com", "pass"));
 		usersMap.put(2, new User(1, "Suresh", "suresh@gmail.com", "pass"));
 	}
 
